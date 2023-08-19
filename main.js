9736ec7b40c9eba3c2a8a9037deb721f23ab59cc
FluffyOx <a@aaaa.ac.cn>
2023-08-19 23:14:06 +0800

    修复了已知错误

--- main.js
+++ main.js
@@ -1,27 +1,8 @@
 var disableInputListenerOnce = false;
-
 window.addEventListener("load", function() {
   var domainInput = document.getElementById("domain-input");
   var searchParams = new URLSearchParams(window.location.search);
   var isDirectParam = false;
-
-  var domain = searchParams.get("name");
-    
-  if (!domain) {
-      var path = window.location.pathname;
-      var parts = path.split("/");
-      if (parts.length > 1) {
-          domain = parts[1];
-            //为了优化体验,如果需要,在这里进一步处理domain
-            isDirectParam = true;
-      }
-  }
-
-
-  if (domain) {
-    domainInput.value = domain;
-    performDNSQuery();
-  }
 
   domainInput.focus();
 
--- preload.js
+++ preload.js
@@ -1,10 +1,12 @@
+
 utools.onPluginEnter(({ type, payload }) => {
     if (payload === "DNS" || payload === "Fluffy DNS" || payload === "DNS记录查询" || payload === "域名解析查询") {
         payload = "";
     }
-    var tableBody = document.querySelector("#result-table tbody");
-    tableBody.innerHTML = "";
-    disableInputListenerOnce = true;
-    document.getElementById('domain-input').value = extractDomain(payload);
-    setTimeout(performDNSQuery,256);
+    document.querySelector("#result-table tbody").innerHTML = "";
+    document.getElementById("domain-input").value = "";
+    setTimeout(function (){
+        document.getElementById("domain-input").value = extractDomain(payload);
+        performDNSQuery();
+    },700);
 })
